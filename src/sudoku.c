765cdea7b2bbe127c2c404b1508a3e85bf571576
hejmanh <10422009@student.vgu.edu.vn>
2023-12-31 23:58:23 +0700

    submission

--- src/sudoku.c
+++ src/sudoku.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 
 #include "hidden_singles.h"
+#include "hidden_singles.h"
+#include "naked_pairs.h"
+#include "naked_triples.h"
+#include "hidden_pairs.h"
 
 int main(int argc, char **argv)
 {
@@ -34,6 +38,24 @@ int main(int argc, char **argv)
         if (solved_counter)
         {
             printf("hidden_singles %d\n", solved_counter);
+            continue;
+        }
+        solved_counter = naked_pairs(board);
+        if (solved_counter)
+        {
+            printf("naked_pairs %d\n", solved_counter);
+            continue;
+        }
+        solved_counter = hidden_pairs(board);
+        if (solved_counter)
+        {
+            printf("hidden_pairs %d\n", solved_counter);
+            continue;
+        }
+        solved_counter = naked_triples(board);
+        if (solved_counter)
+        {
+            printf("naked_tripples %d\n", solved_counter);
             continue;
         }
         
